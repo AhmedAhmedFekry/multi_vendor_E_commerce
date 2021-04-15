c5244d1d50f46aa13e01fdde9def10db7382016a
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-04-15 09:59:58 +0200

    make show order and edit vendor

--- vendor/views.py
+++ vendor/views.py
@@ -28,28 +28,26 @@ def become_vendor(request):
     return render(request, 'vendor/become_vendor.html', {'form': form})
 
 
-login_required
-
-
+@login_required
 def vendor_admin(request):
     vendor = request.user.vendor
     products = vendor.products.all()
-    # orders = vendor.orders.all()
+    orders = vendor.orders.all()
 
-    # for order in orders:
-    #     order.vendor_amount = 0
-    #     order.vendor_paid_amount = 0
-    #     order.fully_paid = True
+    for order in orders:
+        order.vendor_amount = 0
+        order.vendor_paid_amount = 0
+        order.fully_paid = True
 
-    #     for item in order.items.all():
-    #         if item.vendor == request.user.vendor:
-    #             if item.vendor_paid:
-    #                 order.vendor_paid_amount += item.get_total_price()
-    #             else:
-    #                 order.vendor_amount += item.get_total_price()
-    #                 order.fully_paid = False
+        for item in order.items.all():
+            if item.vendor == request.user.vendor:
+                if item.vendor_paid:
+                    order.vendor_paid_amount += item.get_total_price()
+                else:
+                    order.vendor_amount += item.get_total_price()
+                    order.fully_paid = False
 
-    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products})
+    return render(request, 'vendor/vendor_admin.html', {'vendor': vendor, 'products': products, 'orders': orders})
 
 
 @login_required
@@ -68,3 +66,33 @@ def add_product(request):
         form = ProductForm()
 
     return render(request, 'vendor/add_product.html', {'form': form})
+
+
+@login_required
+def edit_vendor(request):
+    vendor = request.user.vendor
+
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+
+        if name:
+            vendor.created_by.email = email
+            vendor.created_by.save()
+
+            vendor.name = name
+            vendor.save()
+
+            return redirect('vendor_admin')
+
+    return render(request, 'vendor/edit_vendor.html', {'vendor': vendor})
+
+# def vendors(request):
+#     vendors = Vendor.objects.all()
+
+#     return render(request, 'vendor/vendors.html', {'vendors': vendors})
+
+# def vendor(request, vendor_id):
+#     vendor = get_object_or_404(Vendor, pk=vendor_id)
+
+#     return render(request, 'vendor/vendor.html', {'vendor': vendor})
